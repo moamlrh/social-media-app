7eb3e08b3686e44456d18deda9efa85a8d7c9bed
moamlrh <moamll288@gmail,com>
2021-03-11 16:20:44 +0300

    add first time to git

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_APPKEY,
+    authDomain: process.env.REACT_APP_AUTH,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_MESSAGE,
+    appId: process.env.REACT_APP_APPID
+});
+
+
+
+export default firebase;
