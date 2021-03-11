d0bd16400e1ff78a3606bb6ca8420aa4c87d74cb
moamlrh <moamll288@gmail,com>
2021-03-11 16:55:38 +0300

    create components file and navbar component

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from '@material-ui/core'
+import React from 'react'
+
+export function Navbar() {
+    return (
+        <AppBar>
+            <Toolbar>
+                Hi
+            </Toolbar>
+        </AppBar>
+    )
+}
