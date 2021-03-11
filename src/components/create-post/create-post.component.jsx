036b7abdd381787390f61436c318c4a8c518ff5b
moamlrh <moamll288@gmail,com>
2021-03-11 20:22:07 +0300

    create post component initialize it

---
+++ src/components/create-post/create-post.component.jsx
@@ -0,0 +1,12 @@
+import { Paper, Typography } from "@material-ui/core";
+import React from "react";
+
+export function CreatePost() {
+  return (
+    <Paper>
+      <Typography variant="h4" component="h4">
+        Add New Post
+      </Typography>
+    </Paper>
+  );
+}
